c661b59115dac2a5b358c5d977be22f24afb56a3
1DimBeam1 <pro100dima_2016@mail.ru>
2025-06-27 23:48:22 +1000

    second clean trash

--- obj/Debug/net8.0/SKAT_Interface.AssemblyInfo.cs
+++ obj/Debug/net8.0/SKAT_Interface.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SKAT_Interface")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+656963c2ec12598a334c8cb42c0c3cf1bbba0d5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06bf338c57919a947ed33565b0938e91ffa489cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("SKAT_Interface")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SKAT_Interface")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
